467cfa0e9241a24f99e107a3f6c941df7c61156f
Jonas <jonas.2022310916@aluno.iffar.edu.br>
2024-07-22 15:55:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# topicos_j
+ 
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="logar.php" method="post">
+        <label>Usuário: <input type="email" name="text"><br><br></label>
+        <label>Email: <input type="email" name="email"></label><br><br>
+        <label>Senha: <input type="password" name="senha"></label><br><br>
+        <input type="submit" value="Login">
+    </form><br><br>
+    <a href="">Cadastrar</a><br><br>
+    <a href="">Recuperar Senha</a>
+</body>
+
+</html>
