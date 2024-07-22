35e0912da2381b1c761acce105b09ceacc61635d
Jonas <jonas.2022310916@aluno.iffar.edu.br>
2024-07-22 16:32:48 -0300

    Login do usuario

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <input type="submit" value="Login">
     </form><br><br>
     <a href="cadastrar-form.php">Cadastrar</a><br><br>
-    <a href="">Recuperar Senha</a>
+    <a href="logar.php">Recuperar Senha</a>
 </body>
 
 </html>
---
+++ logar.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include('conecta.php');
+$usuario_login = $_POST['usuario'];
+$senha = $_POST['senha'];
+if (empty($_POST['usuario']) || empty($_POST['senha'])) {
+	header('Location: index.php');
+	echo "";
+	exit();
+}
+
+$conexao = conectar();
+
+$sql = "SELECT * FROM usuario WHERE usuario='$usuario_login' AND senha='$senha'";
+$resultado = executarSQL($conexao, $sql);
+$usuario = mysqli_fetch_assoc($resultado);
+
+$row = mysqli_num_rows($resultado);
+
+if ($row == 1) {
+	$_SESSION['usuario'] = $usuario;
+	header('Location: principal.php');
+	exit();
+}
+else {
+	header('Location: index.php');
+}
