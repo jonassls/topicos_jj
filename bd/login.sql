3484596ae4e75b97d5a9a18671d0f1330d272b2c
Jonas <jonas.2022310916@aluno.iffar.edu.br>
2024-08-11 22:35:11 -0300

    pronto

--- bd/login.sql
+++ bd/login.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 29-Jul-2024 às 19:09
--- Versão do servidor: 8.0.31
--- versão do PHP: 8.0.26
+-- Tempo de geração: 12/08/2024 às 01:34
+-- Versão do servidor: 8.3.0
+-- Versão do PHP: 8.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Estrutura para tabela `usuario`
 --
 
 DROP TABLE IF EXISTS `usuario`;
@@ -35,14 +35,15 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `senha` varchar(255) NOT NULL,
   `foto_perfil` varchar(255) NOT NULL,
   PRIMARY KEY (`id_usuario`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `usuario`
+-- Despejando dados para a tabela `usuario`
 --
 
 INSERT INTO `usuario` (`id_usuario`, `usuario`, `email`, `senha`, `foto_perfil`) VALUES
-(1, 'aa', 'jonas.202232210916@aluno.iffar.edu.br', '22', '');
+(1, 'Jonas', 'jonas.2022310916@aluno.iffar.edu.br', '22', '66b965cd3c93a.png'),
+(2, 'aa', 'fritasgratis.com@gmail.com', '22', 'padrao.png');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
